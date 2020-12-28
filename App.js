03863b53a186458cc4098d3e3a8456084292d48e
Your Name <you@example.com>
2020-12-28 17:06:28 -0400

    Calculadora de Gorjeta

--- App.js
+++ App.js
@@ -1,42 +1,83 @@
-import React from 'react';
-import { SafeAreaView, Text, View, StyleSheet } from 'react-native';
+import React, { useState } from 'react';
+import { Text, View, Button } from 'react-native';
 import styled from 'styled-components/native';
 
 const Page = styled.SafeAreaView`
     flex:1;
-`;
-const Header = styled.View`
-  flex-direction:row;
-  justify-content:center;
-  flex-wrap:wrap;
-  background-color:#CCC;
-  height:200px;
+    align-self:center;
 `;
 
-const Quadrado = styled.View`
-  width:50px;
-  height:50px;
-  background-color:${props => props.cor};
+const HeaderText = styled.Text`
+  font-size:18px;
+`;
+
+const Input = styled.TextInput`
+  height:45px;
+  font-size:14px;
+  background-color:#EEE;
+  margin-top:20px;
+  border-radius:10px;
+  padding:10px;
+`;
+const CalcButton = styled.Button`
+  margin-top:10px;
+`;
+const ResultArea = styled.View`
+  margin-top:30px;
+  background-color:#EEE;
+  padding:20px;
+  justify-content:center;
+  align-items:center;
+`;
+
+const ResultItemTitle = styled.Text`
+  font-size:14px;
+  font-weight:bold;
+`;
+
+const ResultItem = styled.Text`
+  font-size:15px;
+  margin-bottom:30px;
 `;
 
 export default () => {
+
+  const [bill, setBill] = useState('');
+  const [tip, setTip] = useState(0);
+
+  const calc = () => {
+    let nBill = parseFloat(bill);
+
+    if(nBill) {
+      setTip( (10/100) * nBill );
+    } else {
+      alert("Digite o valor da conta!");
+    }
+  }
+
   return (
     <Page>
-      <Header>
-        <Quadrado cor="red"></Quadrado>
-        <Quadrado cor="yellow"></Quadrado>
-        <Quadrado cor="blue"></Quadrado>
-        <Quadrado cor="pink"></Quadrado>
-        <Quadrado cor="red"></Quadrado>
-        <Quadrado cor="yellow"></Quadrado>
-        <Quadrado cor="blue"></Quadrado>
-        <Quadrado cor="pink"></Quadrado>
-        <Quadrado cor="red"></Quadrado>
-        <Quadrado cor="yellow"></Quadrado>
-        <Quadrado cor="blue"></Quadrado>
-        <Quadrado cor="pink"></Quadrado>
-      </Header>
+      <HeaderText>Calculadora de Gorjeta</HeaderText>
+      <Input
+        placeholder="Valor da conta"
+        placeholderTextColor='#000'
+        keyboardType="numeric"
+        value={bill}
+        onChangeText={n=>setBill(n)}
+      />
+      <CalcButton title="CALCULAR" onPress={calc} />
+      {tip > 0 &&
+        <ResultArea>
+          <ResultItemTitle>Valor da Conta</ResultItemTitle>
+          <ResultItem>R$ {parseFloat(bill).toFixed(2)}</ResultItem>
 
+          <ResultItemTitle>Valor da Gorjeta</ResultItemTitle>
+          <ResultItem>R$ {tip.toFixed(2)}</ResultItem>
+
+          <ResultItemTitle>Valor Total</ResultItemTitle>
+          <ResultItem>R$ {parseFloat(bill) + tip.toFixed(2)}</ResultItem>
+        </ResultArea>
+      }      
     </Page>
   );
 }
