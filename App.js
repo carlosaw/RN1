4f7cd6c71981d15102f304a36e3a6e6e5a4d56b2
carlosaw <carlosfreevg@gmail.com>
2021-01-01 18:59:29 -0400

    Listagem

--- App.js
+++ App.js
@@ -1,34 +1,45 @@
-import React, { Component } from 'react';
-import { Container, Header, Content, Button, Text } from 'native-base';
-export default class ButtonRoundedExample extends Component {
-  render() {
-    return (
-      <Container>
-        <Header />
-        <Content>
-          <Button rounded light>
-            <Text>Light</Text>
-          </Button>
-          <Button rounded>
-            <Text>Primary</Text>
-          </Button>
-          <Button rounded success>
-            <Text>Success</Text>
-          </Button>
-          <Button rounded info>
-            <Text>Info</Text>
-          </Button>
-          <Button rounded warning>
-            <Text>Warning</Text>
-          </Button>
-          <Button rounded danger>
-            <Text>Danger</Text>
-          </Button>
-          <Button rounded dark>
-            <Text>Dark</Text>
-          </Button>
-        </Content>
-      </Container>
-    );
-  }
+import React from 'react';
+import styled from 'styled-components/native';
+import lista from './src/lista';
+
+const Page = styled.SafeAreaView`
+    flex:1;    
+`;
+const Listagem = styled.ScrollView`
+  flex:1;
+`;
+const Item = styled.TouchableOpacity`
+  padding:10px;
+  background-color:#CCC;
+  flex-direction:row;
+`;
+const ItemText = styled.Text`
+  font-size:15px;
+  flex:1;
+`;
+const ItemCheck = styled.View`
+  width:20px;
+  height:20px;
+  border-radius:10px;
+  border:3px solid #FFF;
+`;
+
+export default () => {
+  return (
+    <Page>
+      <Listagem>
+        {lista.map((item, index) => {
+          return (
+
+            <Item key={index} onPress={() => { }} activeOpacity={0.7}>
+              <>
+                <ItemText>{item.task}</ItemText>
+                <ItemCheck></ItemCheck>
+              </>
+            </Item>
+          );
+        })}
+      </Listagem>
+    </Page>
+  );
 }
---
+++ src/lista.js
@@ -0,0 +1,22 @@
+
+export default [
+    { task: 'Fazer XYZ', done: false },
+    { task: 'Fazer ABC', done: true },
+    { task: 'Fazer YUI', done: false },
+    { task: 'Fazer XYZ', done: false },
+    { task: 'Fazer ABC', done: true },
+    { task: 'Fazer YUI', done: false },
+    { task: 'Fazer XYZ', done: false },
+    { task: 'Fazer ABC', done: true },
+    { task: 'Fazer YUI', done: false },
+    { task: 'Fazer XYZ', done: false },
+    { task: 'Fazer ABC', done: true },
+    { task: 'Fazer YUI', done: false },
+    { task: 'Fazer XYZ', done: false },
+    { task: 'Fazer ABC', done: true },
+    { task: 'Fazer YUI', done: false },
+    { task: 'Fazer XYZ', done: false },
+    { task: 'Fazer ABC', done: true },
+    { task: 'Fazer YUI', done: false },
+
+];
